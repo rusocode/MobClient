cead058cf86c718985a515d33e0471b144648935
Juan Debenedetti <juandebenedetti94@gmail.com>
2022-06-23 19:46:49 -0300

    update

--- core/src/com/mob/client/Game.java
+++ core/src/com/mob/client/Game.java
@@ -1,19 +1,3 @@
-/*******************************************************************************
- * Copyright (C) 2014  Rodrigo Troncoso
- *
- *     This program is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU Affero General Public License as
- *     published by the Free Software Foundation, either version 3 of the
- *     License, or (at your option) any later version.
- *
- *     This program is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU Affero General Public License for more details.
- *
- *     You should have received a copy of the GNU Affero General Public License
- *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *******************************************************************************/
 package com.mob.client;
 
 import com.badlogic.gdx.ApplicationListener;
@@ -44,7 +28,7 @@ public class Game implements ApplicationListener {
     @Override
     public void create() {
         // Init spritebatch
-        this.spriteBatch = new SpriteBatch();
+        spriteBatch = new SpriteBatch();
 
         // Load resources
         AssetsHandler.load();
